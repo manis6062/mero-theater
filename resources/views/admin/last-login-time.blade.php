26b1180a4aad0117e893cd30aa05324a38c821b8
DELL <es.androiddeveloper733@gmail.com>
2018-03-27 11:20:31 +0545

    Last login blade changed

--- resources/views/admin/last-login-time.blade.php
+++ resources/views/admin/last-login-time.blade.php
@@ -1,6 +1,11 @@
-@php $numOfSeconds = \Carbon\Carbon::now()->diffInSeconds(\Carbon\Carbon::parse(\Illuminate\Support\Facades\Auth::guard('admin')->user()->login_time)); @endphp
-@php $seconds = $numOfSeconds % 60; @endphp
-@php $numOfMinutes = ($numOfSeconds-$seconds) / 60; @endphp
-@php $minutes = ($numOfMinutes) % 60; @endphp
-@php $hours = abs(($numOfMinutes) / 60); @endphp
-<span class="last-login">Last Login: {{(floor($hours) > 0) ? floor($hours) . ' hours ' .$minutes.' minutes ago' : $minutes.' minutes ago'}}</span>
+@php $lastLoginTime = \Illuminate\Support\Facades\Auth::guard('admin')->user()->last_login_time; @endphp
+@if($lastLoginTime != null)
+    @php $lastLoginTimeArray = array_map('trim', explode(' ',$lastLoginTime)); @endphp
+    @php $lastLoginDate = $lastLoginTimeArray[0]; @endphp
+    @php $lastLoginTime = $lastLoginTimeArray[1]; @endphp
+    @php $displayLastLoginDate = date('M d, Y', strtotime($lastLoginDate)); @endphp
+    @php $displayLastLoginTime = date('h:i A', strtotime($lastLoginTime)); @endphp
+    <span class="last-login">Last Login: {{$displayLastLoginDate.' '.$displayLastLoginTime}}</span>
+@else
+    <span class="last-login">Last Login: No last login data found !</span>
+@endif
